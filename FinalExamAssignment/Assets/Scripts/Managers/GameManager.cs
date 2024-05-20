0491d8ff19069065bd046cc87b437ae7b8cde8c5
Lu-Wannell <2727686@students.wits.ac.za>
2024-05-20 11:31:36 +0200

    added the scripts i have access to

---
+++ FinalExamAssignment/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public Gamestate State;
+
+    public static event Action<Gamestate> OnGameStateChanged;
+
+    public void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        UpdateGameState(Gamestate.StartMenu);
+    }
+
+    public void UpdateGameState(Gamestate newstate)
+    {
+        State = newstate;
+
+        switch (newstate)
+        {
+            case Gamestate.StartMenu:
+                HandleStartMenu();
+                break;
+            case Gamestate.VanessaTurn:
+                HandleVanessaTurn();
+                break;
+            case Gamestate.SwitchScreen:
+                HandleSwitchScreen();
+                break;
+            case Gamestate.AnimatronicTurn:
+                HandleAnimatronicTurn();
+                break;
+            case Gamestate.VanessaVictory:
+                break;
+            case Gamestate.VanessaLoss:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newstate), newstate, null);
+        }
+
+        OnGameStateChanged?.Invoke(newstate);
+    }
+
+
+    public void HandleStartMenu()
+    {
+
+    }
+
+    public void HandleVanessaTurn()
+    {
+
+    }
+
+    public void HandleSwitchScreen()
+    {
+
+    }
+
+    public void HandleAnimatronicTurn()
+    {
+
+    }
+
+}
+
+
+
+public enum Gamestate
+{
+    StartMenu,
+    VanessaTurn,
+    SwitchScreen,
+    AnimatronicTurn,
+    VanessaVictory,
+    VanessaLoss
+}
