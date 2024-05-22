e705a1cef3c98d25c0d6e8f09efebd1ad34c9e75
AraceliN <aryapillay59@gmail.com>
2024-05-22 03:40:48 +0200

    Attempt at movement points & addition of room text

--- FinalExamAssignment/Assets/Scripts/Movement/VPlayerMovement.cs
+++ FinalExamAssignment/Assets/Scripts/Movement/VPlayerMovement.cs
@@ -1,102 +1,80 @@
 using System.Collections;
-
 using System.Collections.Generic;
-
+using TMPro;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 
 
 public class VPlayerMovement : MonoBehaviour
 
 {
-
     public Rigidbody2D Rb;
-
+    public Tilemap floorTilemap;
     public float playerSpeed = 4f;
-
-
-
     Vector2 movement;
+    public TextMeshProUGUI ActionPoints;
+    public int actionPoints;
 
-
-
-    bool facingRight = true;
-
-
+    public void Start()
+    {
+        actionPoints = 5;
+        ActionPoints = GetComponent<TextMeshProUGUI>();
+    }
 
     void Update()
 
     {
-
         movement.x = Input.GetAxisRaw("Horizontal");
-
         movement.y = Input.GetAxisRaw("Vertical");
 
-
-
         if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
-
         {
-
             movement.y = 0;
-
         }
 
         else
-
         {
-
             movement.x = 0;
-
-        }
-
-
-
-        if (movement.x > 0 && !facingRight)
-
-        {
-
-            Flip();
-
-        }
-
-
-
-        if (movement.x < 0 && facingRight)
-
-        {
-
-            Flip();
-
         }
 
     }
 
+    private void Move(Vector2 direction)
+    {
+        if (PlayerMove(direction))
+        {
+            actionPoints -= 1;
+            ActionPoints.text = actionPoints.ToString();
+        }
+    }
 
+    private bool PlayerMove(Vector2 direction)
+    {
+        Vector3Int gridPosition = floorTilemap.WorldToCell(transform.position + (Vector3)direction);
+        if(direction.x > 0)
+        {
+            actionPoints -= 1;
+            ActionPoints.text = actionPoints.ToString();
+            return true;
+        }
+        else if(direction.y > 0)
+        {
+            actionPoints -= 1;
+            ActionPoints.text = actionPoints.ToString();
+            return true;
+        }
+        else
+        {
+            actionPoints = 0;
+            ActionPoints.text = actionPoints.ToString();
+            return false;
+        }
+    }
 
     private void FixedUpdate()
-
     {
-
         Rb.MovePosition(Rb.position + movement * playerSpeed * Time.fixedDeltaTime);
-
-    }
-
-
-
-    void Flip()
-    {
-
-        Vector3 currentScale = gameObject.transform.localScale;
-
-        currentScale.x *= -1;
-
-        gameObject.transform.localScale = currentScale;
-
-
-
-        facingRight = !facingRight;
-
     }
 
 }
