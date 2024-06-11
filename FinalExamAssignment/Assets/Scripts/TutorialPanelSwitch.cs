855f22e11312ae1ab6548bd328deb7b8eb87d71d
Njabulo Hlatshwayo <njabuloayanda17@gmail.com>
2024-06-12 00:46:57 +0200

    final

--- FinalExamAssignment/Assets/Scripts/TutorialPanelSwitch.cs
+++ FinalExamAssignment/Assets/Scripts/TutorialPanelSwitch.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class TutorialPanelSwitch : MonoBehaviour
 {
+    public GameObject panel0;
     public GameObject panel1;
     public GameObject panel2;
     public GameObject panel3;
     public GameObject panel4;
     public GameObject panel5;
     public GameObject panel6;
+    public GameObject VanessaStartTurnButton;
  
     
     void Start()
@@ -20,6 +22,48 @@ public class TutorialPanelSwitch : MonoBehaviour
         panel4.SetActive(false);
         panel5.SetActive(false);
         panel6.SetActive(false);
+    }
+
+    public void ToPage1()
+    {
+        panel0.SetActive(false);
+        panel1.SetActive(true);
+    }
+
+    public void ToPage2()
+    {
+        panel1.SetActive(false);
+        panel2.SetActive(true);
+    }
+
+    public void ToPage3()
+    {
+        panel2.SetActive(false);
+        panel3.SetActive(true);
+    }
+
+    public void ToPage4()
+    {
+        panel3.SetActive(false);
+        panel4.SetActive(true);
+    }
+
+    public void ToPage5()
+    {
+        panel4.SetActive(false);
+        panel5.SetActive(true);
+    }
+
+    public void ToPage6()
+    {
+        panel5.SetActive(false);
+        panel6.SetActive(true);
+    }
+
+    public void StartPlaying()
+    {
+        panel6.SetActive(false);
+        VanessaStartTurnButton.SetActive(true);
     }
 
     
