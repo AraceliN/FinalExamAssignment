0c9a6383cb50b40e2ca3f3ab2a7951c5da8bd602
Lu-Wannell <2727686@students.wits.ac.za>
2024-06-13 12:11:37 +0200

    Update RoundTracker.cs

--- FinalExamAssignment/Assets/Scripts/RoundTracker.cs
+++ FinalExamAssignment/Assets/Scripts/RoundTracker.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class RoundTracker : MonoBehaviour
 {
+    public int roundNumber;
+
+    public GameObject EndAnimatronicTurnButton;
+    public TextMeshProUGUI roundNumberTxt;
+    public bool isVanessaTurn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        roundNumber = 1;
+        isVanessaTurn = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void VanessaTurn()
     {
-        
+        isVanessaTurn = true;
+    }
+
+    public void AnimatronicTurn()
+    {
+        isVanessaTurn = false;
+    }
+
+
+
+    public void NextRound()
+    {
+        roundNumber += 1;
+        roundNumberTxt.text = roundNumber.ToString("");
+
     }
 }
