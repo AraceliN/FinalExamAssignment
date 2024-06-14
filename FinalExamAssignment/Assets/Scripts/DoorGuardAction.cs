e818dccf85f6330cb3c8c77d82fb145773cb8049
Njabulo Hlatshwayo <njabuloayanda17@gmail.com>
2024-06-14 02:02:03 +0200

    More scripting for doors action
    You can only select two options from the dropdown, then itll disappear and set the guard actions button active

--- FinalExamAssignment/Assets/Scripts/DoorGuardAction.cs
+++ FinalExamAssignment/Assets/Scripts/DoorGuardAction.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DoorGuardAction : MonoBehaviour
@@ -35,16 +36,40 @@ public class DoorGuardAction : MonoBehaviour
     public GameObject Entrance_bottom;
     public GameObject Entrance_left;
     public GameObject Restrooms_left;
-    public GameObject Restrooms_right;
+    public GameObject Restrooms_rightTop;
+    public GameObject Restrooms_rightBottom;
     public GameObject Restrooms_top;
     public GameObject Restrooms_bottom;
     public GameObject Unisex_bottom;
     public GameObject Girls_left;
     public GameObject Boys_left;
+    public GameObject Dropdown;
+    public GameObject GuardActionsButton;
 
     int numberofSelections;
     int maxSelections = 2;
     bool selected2 = false;
+
+
+    private void Update()
+    {
+        if (maxSelections == numberofSelections)
+        {
+            selected2 = true;
+        }
+    }
+
+    public void Reset()
+    {
+        if (selected2 == true)
+        {
+            Dropdown.SetActive(false);
+            GuardActionsButton.SetActive(true);
+            numberofSelections = 0;
+            selected2 = false;
+        }
+    }
+
 
     private void Start()
     {
@@ -80,11 +105,55 @@ public class DoorGuardAction : MonoBehaviour
         Entrance_left.SetActive(false);
         Restrooms_bottom.SetActive(false);
         Restrooms_left.SetActive(false); 
-        Restrooms_right.SetActive(false);
+        Restrooms_rightTop.SetActive(false);
+        Restrooms_rightBottom.SetActive(false);
         Restrooms_top.SetActive(false);
         Unisex_bottom.SetActive(false);
         Girls_left.SetActive(false);
         Boys_left.SetActive(false);
+    }
+
+    public void Selectdoors(int index)
+    {
+        switch (index)
+        {
+            case 0:
+               
+
+            case 1:
+                if (selected2 == false)
+                {
+                    numberofSelections++;
+                    Kitchen_left.SetActive(true);
+                    Update();
+                    Reset();
+
+                }
+                break;
+
+                case 2:
+                if (selected2 == false)
+                {
+                    numberofSelections++;
+                    Kitchen_top.SetActive(true);
+                    Update();
+                    Reset();
+
+                }
+                break;
+
+                case 3:
+                if (selected2 == false)
+                {
+                    numberofSelections++;
+                    EastHall_right.SetActive(true);
+                    Update();
+                    Reset();
+
+                }
+                break;
+
+        }
     }
 
 
