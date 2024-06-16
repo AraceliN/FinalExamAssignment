042b72085ac32d0aefc0da76781116f917e8dc31
AFigmentofyourImagination <elrigreyling1@gmail.com>
2024-06-16 12:23:51 +0200

    limiting moves to an amount

--- FinalExamAssignment/Assets/Scripts/Movement/VPlayerMovement.cs
+++ FinalExamAssignment/Assets/Scripts/Movement/VPlayerMovement.cs
@@ -1,102 +1,63 @@
 using System.Collections;
-
 using System.Collections.Generic;
-
 using UnityEngine;
 
-
-
 public class VPlayerMovement : MonoBehaviour
-
 {
-
     public Rigidbody2D Rb;
-
-    public float playerSpeed = 4f;
-
-
+    public float moveSpeed = 4f;
+    private int movesLeft;
+    public int maxMoves = 10;
+    private Vector2 targetPosition;
 
     Vector2 movement;
-
-
-
     bool facingRight = true;
 
-
+    private void Start()
+    {
+        movesLeft = maxMoves;
+        targetPosition = transform.position;
+    }
 
     void Update()
-
     {
-
-        movement.x = Input.GetAxisRaw("Horizontal");
-
-        movement.y = Input.GetAxisRaw("Vertical");
-
-
-
-        if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
-
+        if (movesLeft > 0 && Vector2.Distance(transform.position, targetPosition) < 0.1f)
         {
-
-            movement.y = 0;
-
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                targetPosition += Vector2.up;
+                movesLeft--;
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                targetPosition += Vector2.down;
+                movesLeft--;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                targetPosition += Vector2.left;
+                movesLeft--;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                targetPosition += Vector2.right;
+                movesLeft--;
+            }
         }
 
-        else
-
-        {
-
-            movement.x = 0;
-
-        }
-
-
-
-        if (movement.x > 0 && !facingRight)
-
-        {
-
-            Flip();
-
-        }
-
-
-
-        if (movement.x < 0 && facingRight)
-
-        {
-
-            Flip();
-
-        }
-
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
     }
 
-
-
-    private void FixedUpdate()
-
+    private void OnGUI()
     {
-
-        Rb.MovePosition(Rb.position + movement * playerSpeed * Time.fixedDeltaTime);
-
+        GUI.Label(new Rect(10, 10, 200, 20), "Moves Left: " + movesLeft);
     }
-
-
 
     void Flip()
     {
-
-        Vector3 currentScale = gameObject.transform.localScale;
-
-        currentScale.x *= -1;
-
-        gameObject.transform.localScale = currentScale;
-
-
-
-        facingRight = !facingRight;
-
+            Vector3 currentScale = gameObject.transform.localScale;
+            currentScale.x *= -1;
+            gameObject.transform.localScale = currentScale;
+            facingRight = !facingRight;
     }
-
 }
