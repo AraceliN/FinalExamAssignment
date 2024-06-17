cba53ec957772244e03913bf7f2ea58a2a4a65a0
Lu-Wannell <2727686@students.wits.ac.za>
2024-06-17 22:13:41 +0200

    added movementPoints

--- FinalExamAssignment/Assets/Scripts/Movement/FrPlayerMovement.cs
+++ FinalExamAssignment/Assets/Scripts/Movement/FrPlayerMovement.cs
@@ -10,56 +10,70 @@ public class FrPlayerMovement : MonoBehaviour
 
 {
 
-    public Rigidbody2D Rb;
 
-    public float playerSpeed = 4f;
+
+    public Rigidbody2D Rb;
+    public float moveSpeed = 4f;
+    private int movesLeft;
+    public int maxMoves = 5;
+    private Vector2 targetPosition;
 
     public GameObject PrizeCornerBlue;
 
     Vector2 movement;
+    bool facingRight = true;
 
 
 
-
-
+    private void Start()
+    {
+        movesLeft = maxMoves;
+        targetPosition = transform.position;
+    }
 
     void Update()
-
     {
-
-        movement.x = Input.GetAxisRaw("Horizontal");
-
-        movement.y = Input.GetAxisRaw("Vertical");
-
-
-
-        if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
-
+        if (movesLeft > 0 && Vector2.Distance(transform.position, targetPosition) < 0.1f)
         {
-
-            movement.y = 0;
-
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                targetPosition += Vector2.up;
+                movesLeft--;
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                targetPosition += Vector2.down;
+                movesLeft--;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                targetPosition += Vector2.left;
+                movesLeft--;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                targetPosition += Vector2.right;
+                movesLeft--;
+            }
         }
 
-        else
-
-        {
-
-            movement.x = 0;
-
-        }
-
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
     }
 
 
-
-    private void FixedUpdate()
-
+    private void OnGUI()
     {
-
-        Rb.MovePosition(Rb.position + movement * playerSpeed * Time.fixedDeltaTime);
-
+        GUI.Label(new Rect(10, 10, 200, 20), "Moves Left: " + movesLeft);
     }
+
+    void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+        facingRight = !facingRight;
+    }
+
 
     public void ResetFreddy()
     {
